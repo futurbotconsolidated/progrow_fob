75b9315ffd1b7c56e0d55d5fad8650390df99806
ArunJH04 <51953327+ArunJH04@users.noreply.github.com>
2022-01-07 10:28:25 +0530

    form-array dynamic implementation

--- src/app/add-farmer/demographic-info/demographic-info.component.ts
+++ src/app/add-farmer/demographic-info/demographic-info.component.ts
@@ -1,7 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { religion } from '../../shared/modal/global-field-values';
-import { gender } from '../../shared/modal/global-field-values';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+  FormArray,
+} from '@angular/forms';
+import {
+  religion,
+  gender,
+  caste,
+  propertyStatus,
+  commOrPerAddress,
+  relation,
+  education,
+  occupation,
+  dependency,
+  ownershipType,
+  particular,
+  phoneType,
+  phoneOperating,
+  agriculturalInterest,
+  cultivationAdvice,
+  adviceMedium,
+  sourceOfIncome,
+  innovativeWaysFarming,
+} from '../../shared/modal/global-field-values';
 
 @Component({
   selector: 'app-demographic-info',
@@ -11,23 +35,165 @@ import { gender } from '../../shared/modal/global-field-values';
 export class DemographicInfoComponent implements OnInit {
   religionList = <any>[];
   genderList = <any>[];
-  demographicInfoForm = new FormGroup({
-    firstName: new FormControl('', [Validators.required]),
-    PANnumber: new FormControl('', [Validators.required]),
-    middleName: new FormControl('', [Validators.required]),
-    lastName: new FormControl('', [Validators.required]),
-    dob: new FormControl('', [Validators.required]),
-    gender: new FormControl('male', [Validators.required]),
-    religion: new FormControl('hindu', [Validators.required]),
-  });
+  casteList = <any>[];
+  propertyStatusList = <any>[];
+  addressStatusList = <any>[];
+  relationList = <any>[];
+  educationList = <any>[];
+  occupationList = <any>[];
+  dependencyList = <any>[];
+  ownershipTypeList = <any>[];
+  particularList = <any>[];
+  phoneTypeList = <any>[];
+  phoneOperatingList = <any>[];
+  agriculturalInterestList = <any>[];
+  cultivationAdviceList = <any>[];
+  adviceMediumList = <any>[];
+  sourceOfIncomeList = <any>[];
+  innovativeWaysFarmingList = <any>[];
 
-  constructor() {}
+  familyMembers!: FormArray;
+  propertyOwnership!: FormArray;
+  demographicInfoForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.demographicInfoForm = this.formBuilder.group({
+      firstName: new FormControl('', [Validators.required]),
+      PANnumber: new FormControl('', [Validators.required]),
+      middleName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      dob: new FormControl('', [Validators.required]),
+      gender: new FormControl('male', [Validators.required]),
+      religion: new FormControl('hindu', [Validators.required]),
+      caste: new FormControl('sc', [Validators.required]),
+      educationQualification: new FormControl('', [Validators.required]),
+      occupation: new FormControl('', [Validators.required]),
+      annualIncome: new FormControl('', [Validators.required]),
+      address1: new FormControl('', [Validators.required]),
+      address2: new FormControl('', [Validators.required]),
+      taluk: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      pinCode: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
+      landmark: new FormControl('', [Validators.required]),
+      phoneNumber: new FormControl('', [Validators.required]),
+      mobile1: new FormControl('', [Validators.required]),
+      mobile2: new FormControl('', [Validators.required]),
+      yrsInAddress: new FormControl('', [Validators.required]),
+      yrsInCity: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      propertyStatus: new FormControl('own', [Validators.required]),
+      monthlyRent: new FormControl('', [Validators.required]),
+      commOrPerAddress: new FormControl('same_above', [Validators.required]),
+      familyMembers: new FormArray([this.createFamilyMembers()]),
+      propertyOwnership: new FormArray([this.createPropertyOwnership()]),
+      phoneType: new FormControl('feature_phone', [Validators.required]),
+      phoneOperating: new FormControl('i_check_mostly', [Validators.required]),
+      cultivationAdvice: [Array()],
+      adviceMedium: [Array()],
+      sourceOfIncome: [Array()],
+      agriculturalInterest: new FormControl('very_much_interested', [
+        Validators.required,
+      ]),
+      innovativeWaysFarming: [Array()],
+    });
+  }
 
   ngOnInit(): void {
     this.religionList = religion;
     this.genderList = gender;
-    console.log(this.religionList);
+    this.casteList = caste;
+    this.propertyStatusList = propertyStatus;
+    this.addressStatusList = commOrPerAddress;
+    this.relationList = relation;
+    this.occupationList = occupation;
+    this.educationList = education;
+    this.dependencyList = dependency;
+    this.ownershipTypeList = ownershipType;
+    this.particularList = particular;
+    this.phoneTypeList = phoneType;
+    this.phoneOperatingList = phoneOperating;
+    this.cultivationAdviceList = cultivationAdvice;
+    this.adviceMediumList = adviceMedium;
+    this.sourceOfIncomeList = sourceOfIncome;
+    this.agriculturalInterestList = agriculturalInterest;
+    this.innovativeWaysFarmingList = innovativeWaysFarming;
   }
+  createPropertyOwnership(): FormGroup {
+    return this.formBuilder.group({
+      propertyType: new FormControl('', [Validators.required]),
+      propertyPic: new FormControl('Relation', [Validators.required]),
+      ownershipType: new FormControl('Education', [Validators.required]),
+      particular: new FormControl('Occupation', [Validators.required]),
+      cumulativeValue: new FormControl('Dependency', [Validators.required]),
+    });
+  }
+
+  getPropertyOwnershipControls() {
+    return (this.demographicInfoForm.get('propertyOwnership') as FormArray)
+      .controls;
+  }
+
+  addPropertyOwnership(): void {
+    this.propertyOwnership = this.demographicInfoForm.get(
+      'propertyOwnership'
+    ) as FormArray;
+    this.propertyOwnership.push(this.createPropertyOwnership());
+  }
+
+  removePropertyOwnership(index: any) {
+    this.propertyOwnership.removeAt(index);
+  }
+
+  createFamilyMembers(): FormGroup {
+    return this.formBuilder.group({
+      name: new FormControl('', [Validators.required]),
+      relation: new FormControl('Relation', [Validators.required]),
+      education: new FormControl('Education', [Validators.required]),
+      occupation: new FormControl('Occupation', [Validators.required]),
+      dependency: new FormControl('Dependency', [Validators.required]),
+    });
+  }
+
+  getFamilyMembersControls() {
+    return (this.demographicInfoForm.get('familyMembers') as FormArray)
+      .controls;
+  }
+
+  addFamilyMembers(): void {
+    this.familyMembers = this.demographicInfoForm.get(
+      'familyMembers'
+    ) as FormArray;
+    this.familyMembers.push(this.createFamilyMembers());
+  }
+
+  removeFamilyMembers(index: any) {
+    this.familyMembers.removeAt(index);
+  }
+
+  selectCultivationAdvice(event: any, formCtlName: any, formVal: any) {
+    formVal = String(formVal);
+    let aryValCurr = this.demographicInfoForm.controls[formCtlName].value;
+    let aryValNew: any = [];
+    if (Array.isArray(aryValCurr)) {
+      aryValNew = [...aryValCurr];
+    } else if (aryValCurr) {
+      aryValNew = String(aryValCurr).split(',');
+    }
+    if (aryValNew.includes(formVal) && !event.target.checked) {
+      aryValNew.splice(aryValNew.indexOf(formVal), 1);
+    } else {
+      aryValNew.push(formVal);
+    }
+    // update the form value
+    // @ts-ignore: Object is possibly 'null'.
+    this.demographicInfoForm.get(formCtlName).setValue(aryValNew);
+    if (this.demographicInfoForm.controls[formCtlName].pristine) {
+      // @ts-ignore: Object is possibly 'null'.
+      this.demographicInfoForm.get(formCtlName).markAsDirty();
+    }
+  }
+
   onSubmit() {
     console.log(this.demographicInfoForm.value);
   }
--- src/app/shared/modal/global-field-values.ts
+++ src/app/shared/modal/global-field-values.ts
@@ -7,31 +7,31 @@ export const religion = [
     defaultValue: 'hindu',
   },
   {
-    id: 1,
+    id: 2,
     displayName: 'Muslim',
     displayValue: 'muslim',
     defaultValue: false,
   },
   {
-    id: 1,
+    id: 3,
     displayName: 'Sikh',
     displayValue: 'sikh',
     defaultValue: false,
   },
   {
-    id: 1,
+    id: 4,
     displayName: 'Christian',
     displayValue: 'Christian',
     defaultValue: false,
   },
   {
-    id: 1,
+    id: 5,
     displayName: 'Zoroastrian',
     displayValue: 'Zoroastrian',
     defaultValue: false,
   },
   {
-    id: 1,
+    id: 6,
     displayName: 'Buddhist',
     displayValue: 'Buddhist',
     defaultValue: false,
@@ -45,14 +45,89 @@ export const gender = [
     displayValue: 'male',
   },
   {
-    id: 1,
+    id: 2,
     displayName: 'Female',
     displayValue: 'female',
   },
   {
-    id: 1,
+    id: 3,
     displayName: 'Third-Gender',
-    displayValue: 'thirdgender',
+    displayValue: 'third_gender',
+  },
+];
+
+export const commOrPerAddress = [
+  {
+    id: 1,
+    displayName: 'Same as above',
+    displayValue: 'same_above',
+    defaultValue: 'same_above',
+  },
+  {
+    id: 2,
+    displayName: 'Different address',
+    displayValue: 'different_address',
+    defaultValue: false,
+  },
+];
+
+export const caste = [
+  {
+    id: 1,
+    displayName: 'SC',
+    displayValue: 'sc',
+    defaultValue: 'sc',
+  },
+  {
+    id: 2,
+    displayName: 'ST',
+    displayValue: 'st',
+    defaultValue: false,
+  },
+  {
+    id: 3,
+    displayName: 'OBC',
+    displayValue: 'obc',
+    defaultValue: false,
+  },
+  {
+    id: 4,
+    displayName: 'General',
+    displayValue: 'general',
+    defaultValue: false,
+  },
+];
+
+export const propertyStatus = [
+  {
+    id: 1,
+    displayName: 'Own',
+    displayValue: 'own',
+    defaultValue: 'own',
+  },
+  {
+    id: 2,
+    displayName: 'Parental',
+    displayValue: 'parental',
+    defaultValue: false,
+  },
+  {
+    id: 3,
+    displayName: 'Rented',
+    displayValue: 'rented',
+    defaultValue: false,
+  },
+  {
+    id: 4,
+    displayName: 'Leased',
+    displayValue: 'leased',
+    defaultValue: false,
+  },
+  {
+    id: 5,
+    displayName: 'Company Provided',
+    displayValue: 'company_provided',
+    defaultValue: false,
   },
 ];
 
@@ -75,10 +150,90 @@ export const salutation = [
   },
 ];
 
+export const phoneType = [
+  { id: 1, displayName: 'Feature Phone', displayValue: 'feature_phone' },
+  { id: 2, displayName: 'Smart Phone', displayValue: 'smart_phone' },
+  {
+    id: 3,
+    displayName: 'No Personal Phone',
+    displayValue: 'no_personal_phone',
+  },
+];
+
+export const phoneOperating = [
+  { id: 1, displayName: 'I Check Mostly', displayValue: 'i_check_mostly' },
+  { id: 2, displayName: 'Somebody Else', displayValue: 'somebody_else' },
+  { id: 3, displayName: 'Shared Phone', displayValue: 'shared_phone' },
+];
+
+export const cultivationAdvice = [
+  {
+    id: 1,
+    displayName: 'Neighbouring farmer/family',
+    displayValue: 'neighbouring_farmer_family',
+  },
+  { id: 2, displayName: 'Company Doctor', displayValue: 'company_doctor' },
+  { id: 3, displayName: 'TV Program', displayValue: 'tv_program' },
+  { id: 3, displayName: 'KVK', displayValue: 'kvk' },
+  { id: 3, displayName: 'Youtube', displayValue: 'youtube' },
+  { id: 3, displayName: 'Facebook', displayValue: 'facebook' },
+];
+
+export const adviceMedium = [
+  { id: 1, displayName: 'SMS', displayValue: 'sms' },
+  { id: 2, displayName: 'Whatsapp', displayValue: 'whatsapp' },
+  { id: 3, displayName: 'Telephone Call', displayValue: 'telephone_call' },
+  { id: 3, displayName: 'Physical Visits', displayValue: 'physical_visits' },
+];
+
+export const sourceOfIncome = [
+  { id: 1, displayName: 'Shop', displayValue: 'shop' },
+  { id: 2, displayName: 'Govt. Service', displayValue: 'govt_service' },
+  {
+    id: 3,
+    displayName: 'Private Service/Self employed',
+    displayValue: 'private_service',
+  },
+  {
+    id: 3,
+    displayName: 'Renting Heavy farm machinery or equipment',
+    displayValue: 'renting_heavy_farm_machinery',
+  },
+  {
+    id: 3,
+    displayName: 'Dairy Income machinery or equipment',
+    displayValue: 'dairy_income_machinery',
+  },
+];
+
+export const agriculturalInterest = [
+  {
+    id: 1,
+    displayName: 'Very much Interested',
+    displayValue: 'very_much_interested',
+  },
+  { id: 2, displayName: 'Interested', displayValue: 'interested' },
+  { id: 3, displayName: 'May be', displayValue: 'may_be' },
+  { id: 4, displayName: 'Not Interested', displayValue: 'not_interested' },
+];
+
+export const innovativeWaysFarming = [
+  { id: 1, displayName: 'Mulch', displayValue: 'mulch' },
+  { id: 2, displayName: 'Solar Pump', displayValue: 'solar_pump' },
+  {
+    id: 3,
+    displayName: 'Water Soluble Fertilizers',
+    displayValue: 'water_soluble_fertilizers',
+  },
+  { id: 3, displayName: 'IoT/Drone', displayValue: 'iot_drone' },
+  { id: 3, displayName: 'Weather Updates', displayValue: 'weather_updates' },
+  { id: 3, displayName: 'SOPs', displayValue: 'sops' },
+];
+
 export const relation = [
   { id: 1, displayName: 'Father', displayValue: 'father' },
-  { id: 1, displayName: 'Mother', displayValue: 'mother' },
-  { id: 1, displayName: 'Sister', displayValue: 'sister' },
+  { id: 2, displayName: 'Mother', displayValue: 'mother' },
+  { id: 3, displayName: 'Sister', displayValue: 'sister' },
 ];
 export const education = [
   { id: 1, displayName: '10th', displayValue: '10th' },
