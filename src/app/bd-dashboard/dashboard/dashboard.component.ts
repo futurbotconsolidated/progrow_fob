682fd71a17e2b76cc19213af80bc0a538e1c2d59
checkwithak <checkwithak@gmail.com>
2022-01-18 08:43:36 +0530

    user info in dashoard

--- src/app/bd-dashboard/dashboard/dashboard.component.ts
+++ src/app/bd-dashboard/dashboard/dashboard.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../app.service';
+import { OAuthService } from 'angular-oauth2-oidc';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  constructor(private appService: AppService) {}
+  userInfo: any;
+  constructor(
+    private appService: AppService,
+    public oauthService: OAuthService
+  ) {
+    this.userInfo = this.oauthService.getIdentityClaims();
+
+    console.log(this.userInfo);
+  }
 
   ngOnInit(): void {
     this.appService.getAllFarmersData().subscribe((res: any) => {
-      console.log(res, 'log from dashboard');
+      console.log(res, 'log from dashboards');
     });
   }
 }
