c982654ea99e6a927116fa82601d5ec5c0b3a793
checkwithak <checkwithak@gmail.com>
2022-01-22 14:40:12 +0530

    mapbox

--- src/app/bd-dashboard/dashboard/dashboard.component.ts
+++ src/app/bd-dashboard/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { OAuthService } from 'angular-oauth2-oidc';
 import * as mapboxgl from 'mapbox-gl';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { CommonService } from '../../shared/common.service';
+import { mapData } from '../../../assets/overlay_data';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,11 +13,11 @@ import { CommonService } from '../../shared/common.service';
 export class DashboardComponent implements OnInit {
   /* START: Variables */
   userInfo: any;
+  selectedViewType = 'EXISTING_FARMS_LIST_VIEW';
   allExistingFarmers = [] as any;
   allPipelineFarmers = [] as any;
   overlayData = [] as any;
 
-  selectedViewType = 'EXISTING_FARMS_LIST_VIEW';
   /* END: Variables */
 
   constructor(
@@ -50,10 +51,63 @@ export class DashboardComponent implements OnInit {
   }
 
   overlayMap(type: string) {
-    this.overlayData.length = 0;
-
+    this.spinner.show();
+    this.overlayData.length = 0; // clear data
+    let useData = [] as any;
     if (type == 'EXISTING_FARMS_MAP_VIEW') {
+      useData = mapData['features'];
+    } else if (type == 'FARMS_PIPELINE_MAP_VIEW') {
+      useData = mapData['features'];
     }
+    //  Start Overlay Code
+    // TO MAKE THE MAP APPEAR YOU MUST ADD  YOUR ACCESS TOKEN FROM https://account.mapbox.com
+    const map = new mapboxgl.Map({
+      accessToken:
+        'pk.eyJ1IjoicHVybmFyYW0iLCJhIjoiY2tpenBvZWpsMDNlaTMzcWpiZ2liZjEydiJ9.Mdj1w5dXDfCGCpIH5MlI2g',
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/satellite-v9?optimize=true', // style URL
+      zoom: 14, // starting zoom
+      center: [71.43395031003963, 27.04756708332954],
+    });
+
+    // geojson coordinates
+    map.on('load', () => {
+      console.log('aaaaaaaa');
+      useData.forEach((elem: any, index: number) => {
+        elem['geometry'].coordinates.forEach((h: any, i: number) => {
+          // Add Source
+          map.addSource(`figure${i}_${index}`, {
+            type: 'geojson',
+            data: {
+              type: 'Feature',
+              geometry: {
+                type: 'Polygon',
+                coordinates: [h],
+              },
+            } as any,
+          });
+
+          // Add a layer showing the fields.
+          map.addLayer({
+            id: `figure${i}_${index}`,
+            type: 'fill',
+            source: `figure${i}_${index}`,
+            layout: {},
+            paint: {
+              'fill-color': 'red',
+              'fill-opacity': 0.8,
+            },
+          });
+        });
+
+        if (index == useData.length - 1) {
+          this.spinner.hide();
+        }
+      });
+    });
+
+    map.addControl(new mapboxgl.NavigationControl()); // Add map controls
+    map.addControl(new mapboxgl.FullscreenControl()); // Add map full screen
   }
   /* END: Non-API Function Calls */
 
@@ -85,5 +139,5 @@ export class DashboardComponent implements OnInit {
       }
     );
   }
-  /* END: API Functions Calls */
+  /* END: API Function Calls */
 }
