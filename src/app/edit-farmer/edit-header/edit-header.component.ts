ade77613ddd787c2e11d73b5ca2fbd1f54b5e392
checkwithak <checkwithak@gmail.com>
2022-02-07 23:37:43 +0530

    profile page for demographic,tech adoption, prod aggr, crop market plan

--- src/app/edit-farmer/edit-header/edit-header.component.ts
+++ src/app/edit-farmer/edit-header/edit-header.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-edit-header',
   templateUrl: './edit-header.component.html',
-  styleUrls: ['./edit-header.component.css']
+  styleUrls: ['./edit-header.component.css'],
 })
 export class EditHeaderComponent implements OnInit {
+  demographicDisp = {} as any;
+  constructor() {
+    const A: any = localStorage.getItem('farmer-details');
 
-  constructor() { }
-
-  ngOnInit(): void {
+    if (A) {
+      this.demographicDisp = JSON.parse(A).demographic_info;
+      console.log(this.demographicDisp);
+    }
   }
-
+  ngOnInit(): void {}
 }
