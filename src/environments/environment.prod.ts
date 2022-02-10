f73a9410dbc6ccc2833d024e594fe7f1cfe4d072
checkwithak <checkwithak@gmail.com>
2022-02-10 10:01:05 +0530

    prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
-import { baseUrl, endPoints } from './api-endpoints';
+import { endPoints } from './api-endpoints';
 export const environment = {
   production: true,
-  baseUrl: baseUrl,
+  baseUrl: 'https://api1.agrisaathi.com',
   endPoints: endPoints,
 };
