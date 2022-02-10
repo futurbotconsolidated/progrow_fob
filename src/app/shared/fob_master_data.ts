d50295d985dae0a34081f71d3baba5b945dc2864
checkwithak <checkwithak@gmail.com>
2022-02-10 09:05:32 +0530

    profile

--- src/app/shared/fob_master_data.ts
+++ src/app/shared/fob_master_data.ts
@@ -427,4 +427,126 @@ export const data = {
       },
     ],
   },
+  demoGraphic: {
+    relation: [
+      { id: 1, displayName: 'Father', displayValue: 'father' },
+      { id: 2, displayName: 'Mother', displayValue: 'mother' },
+      { id: 3, displayName: 'Sister', displayValue: 'sister' },
+    ],
+    education: [
+      { id: 1, displayName: '10th', displayValue: '10th' },
+      { id: 2, displayName: 'PUC', displayValue: 'puc' },
+    ],
+    occupation: [
+      { id: 1, displayName: 'Farmer', displayValue: 'farmer' },
+      { id: 2, displayName: 'House Wife', displayValue: 'house_wife' },
+    ],
+    dependency: [
+      { id: 1, displayName: '1', displayValue: '1' },
+      { id: 2, displayName: '2', displayValue: '2' },
+    ],
+    innovativeWaysFarming: [
+      { id: 1, displayName: 'Mulch', displayValue: 'mulch' },
+      { id: 2, displayName: 'Solar Pump', displayValue: 'solar_pump' },
+      {
+        id: 3,
+        displayName: 'Water Soluble Fertilizers',
+        displayValue: 'water_soluble_fertilizers',
+      },
+      { id: 3, displayName: 'IoT/Drone', displayValue: 'iot_drone' },
+      {
+        id: 3,
+        displayName: 'Weather Updates',
+        displayValue: 'weather_updates',
+      },
+      { id: 3, displayName: 'SOPs', displayValue: 'sops' },
+    ],
+    agriculturalInterest: [
+      {
+        id: 1,
+        displayName: 'Very much Interested',
+        displayValue: 'very_much_interested',
+      },
+      { id: 2, displayName: 'Interested', displayValue: 'interested' },
+      { id: 3, displayName: 'May be', displayValue: 'May be' },
+      {
+        id: 4,
+        displayName: 'Not Interested',
+        displayValue: 'not_interested',
+      },
+    ],
+    sourceOfIncome: [
+      { id: 1, displayName: 'Shop', displayValue: 'shop' },
+      { id: 2, displayName: 'Govt. Service', displayValue: 'govt_service' },
+      {
+        id: 3,
+        displayName: 'Private Service/Self employed',
+        displayValue: 'private_service',
+      },
+      {
+        id: 3,
+        displayName: 'Renting Heavy farm machinery or equipment',
+        displayValue: 'renting_heavy_farm_machinery',
+      },
+      {
+        id: 3,
+        displayName: 'Dairy Income machinery or equipment',
+        displayValue: 'dairy_income_machinery',
+      },
+    ],
+    phoneType: [
+      { id: 1, displayName: 'Feature Phone', displayValue: 'feature_phone' },
+      { id: 2, displayName: 'Smart Phone', displayValue: 'smart_phone' },
+      {
+        id: 3,
+        displayName: 'No Personal Phone',
+        displayValue: 'no_personal_phone',
+      },
+    ],
+    phoneOperating: [
+      { id: 1, displayName: 'I Check Mostly', displayValue: 'i_check_mostly' },
+      { id: 2, displayName: 'Somebody Else', displayValue: 'somebody_else' },
+      { id: 3, displayName: 'Shared Phone', displayValue: 'shared_phone' },
+    ],
+    adviceMedium: [
+      { id: 1, displayName: 'SMS', displayValue: 'sms' },
+      { id: 2, displayName: 'Whatsapp', displayValue: 'whatsapp' },
+      { id: 3, displayName: 'Telephone Call', displayValue: 'telephone_call' },
+      {
+        id: 3,
+        displayName: 'Physical Visits',
+        displayValue: 'physical_visits',
+      },
+    ],
+    cultivationAdvice: [
+      {
+        id: 1,
+        displayName: 'Neighbouring farmer/family',
+        displayValue: 'neighbouring_farmer_family',
+      },
+      { id: 2, displayName: 'Company Doctor', displayValue: 'company_doctor' },
+      { id: 3, displayName: 'TV Program', displayValue: 'tv_program' },
+      { id: 3, displayName: 'KVK', displayValue: 'kvk' },
+      { id: 3, displayName: 'Youtube', displayValue: 'youtube' },
+      { id: 3, displayName: 'Facebook', displayValue: 'facebook' },
+    ],
+    propertyType: [
+      { id: 1, displayName: 'House', displayValue: 'house' },
+      { id: 2, displayName: 'Land', displayValue: 'land' },
+    ],
+    ownerShipType: [
+      {
+        id: 1,
+        displayName: 'Leased (Joint/Self)',
+        displayValue: 'Leased (Joint/Self)',
+      },
+      {
+        id: 2,
+        displayName: 'Owned (Joint/Self)',
+        displayValue: 'Owned (Joint/Self)',
+      },
+      { id: 3, displayName: 'Share Cropping', displayValue: 'Share Cropping' },
+    ],
+    particular: [{ id: 1, displayName: 'ABC', displayValue: 'ABC' }],
+  },
 };
