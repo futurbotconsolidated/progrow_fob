d432ea928025c92afc70d3a9c04035a16bf979d2
checkwithak <checkwithak@gmail.com>
2022-02-10 15:46:52 +0530

    api changes

--- src/environments/api-endpoints.ts
+++ src/environments/api-endpoints.ts
@@ -1,5 +1,5 @@
 // export const baseUrl = `http://18.223.180.21`;
-export const baseUrl = `https://api.fob2.dev.adaptiwise.com`;
+export const baseUrl = 'https://api.fob2.dev.adaptiwise.com';
 
 export const endPoints = {
   getAllFarmers: '/getAllFarmers',
