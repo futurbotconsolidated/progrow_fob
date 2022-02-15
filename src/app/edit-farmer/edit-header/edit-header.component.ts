2c417e58896bacb6fae77ed92d42b014829428ec
checkwithak <checkwithak@gmail.com>
2022-02-15 14:21:08 +0530

    bug fixes

--- src/app/edit-farmer/edit-header/edit-header.component.ts
+++ src/app/edit-farmer/edit-header/edit-header.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { OAuthService } from 'angular-oauth2-oidc';
 import { CommonService } from '../../shared/common.service';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-edit-header',
@@ -8,27 +12,78 @@ import { CommonService } from '../../shared/common.service';
   styleUrls: ['./edit-header.component.css'],
 })
 export class EditHeaderComponent implements OnInit {
+  /* START: Variables */
   farmerId = '';
+  farmerData = {} as any;
   demographicDisp = {} as any;
+  currentRoute = '';
+  /* END: Variables */
+
   constructor(
     private activatedRoute: ActivatedRoute,
-    private commonService: CommonService
+    private toastr: ToastrService,
+    public commonService: CommonService,
+    public oauthService: OAuthService,
+    private spinner: NgxSpinnerService,
+    private router: Router
   ) {
-    const A: any = localStorage.getItem('farmer-details');
-    if (A) {
-      this.demographicDisp = JSON.parse(A).demographic_info;
-    }
+    console.log(router.url);
+    this.farmerId = this.activatedRoute.snapshot.params['farmerid'];
+    this.router.events.subscribe((event) => {
+      if (
+        event.constructor.name === 'NavigationEnd' ||
+        event instanceof NavigationEnd
+      ) {
+        const A = this.router.url.replace('/', '').split('/');
+        console.log(A);
+
+        if (A[0] === 'edit' && A[1] === 'demographic-info') {
+          this.loadData();
+        }
+
+        const B = localStorage.getItem('farmer-details');
+        if (B) {
+          this.demographicDisp = JSON.parse(B).demographic_info;
+        }
+      }
+    });
   }
-  ngOnInit(): void {
-    this.loadData();
-  }
+  ngOnInit(): void {}
   /* START: Non-API Function Calls */
   loadData() {
-    this.farmerId = this.activatedRoute.snapshot.params['farmerid'];
+    localStorage.removeItem('farmer-details');
+
+    this.getFarmerDetailsById(this.farmerId);
   }
   // get Name from Master Json
   getDisplayName(dataProperty: string, id: any) {
     return this.commonService.getDisplayName('demoGraphic', dataProperty, id);
   }
   /* END: Non-API Function Calls */
+
+  /* START: API Function Calls */
+  getFarmerDetailsById(farmerId: any) {
+    this.spinner.show();
+    this.commonService.getFarmerDetailsById(farmerId).subscribe(
+      (res: any) => {
+        this.spinner.hide();
+        if (res.message != 'Success' || !res.status) {
+          this.toastr.error(`${res.message}!`);
+        } else {
+          this.farmerData = res.data;
+          localStorage.setItem(
+            'farmer-details',
+            JSON.stringify(this.farmerData)
+          );
+        }
+      },
+      (error: any) => {
+        this.spinner.hide();
+        this.toastr.error(
+          `Failed to fetch farmer details, please try again...`
+        );
+      }
+    );
+  }
+  /* END: API Function Calls */
 }
--- src/app/edit-farmer/summary/summary.component.ts
+++ src/app/edit-farmer/summary/summary.component.ts
@@ -13,52 +13,14 @@ import { ToastrService } from 'ngx-toastr';
 export class SummaryComponent implements OnInit {
   /* START: Variables */
   userInfo: any;
-  farmerData = {} as any;
   /* END: Variables */
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private toastr: ToastrService,
     public commonService: CommonService,
-    public oauthService: OAuthService,
-    private spinner: NgxSpinnerService
+    public oauthService: OAuthService
   ) {
     this.userInfo = this.oauthService.getIdentityClaims();
   }
 
-  ngOnInit(): void {
-    this.loadData();
-  }
-  /* START: Non-API Function Calls */
-  loadData() {
-    const farmerId = this.activatedRoute.snapshot.params['farmerid'];
-    this.getFarmerDetailsById(farmerId);
-  }
-  /* END: Non-API Function Calls */
-
-  /* START: API Function Calls */
-  getFarmerDetailsById(farmerId: any) {
-    this.spinner.show();
-    this.commonService.getFarmerDetailsById(farmerId).subscribe(
-      (res: any) => {
-        this.spinner.hide();
-        if (res.message != 'Success' || !res.status) {
-          this.toastr.error(`${res.message}!`);
-        } else {
-          this.farmerData = res.data;
-          localStorage.setItem(
-            'farmer-details',
-            JSON.stringify(this.farmerData)
-          );
-        }
-      },
-      (error: any) => {
-        this.spinner.hide();
-        this.toastr.error(
-          `Failed to fetch farmer details, please try again...`
-        );
-      }
-    );
-  }
-  /* END: API Function Calls */
+  ngOnInit(): void {}
 }
