4292b3150037d2420f2a3ba8e98a5f4df27ac5e0
checkwithak <checkwithak@gmail.com>
2022-02-16 18:21:43 +0530

    call getFarmerId api only once

--- src/app/bd-dashboard/dashboard/dashboard.component.ts
+++ src/app/bd-dashboard/dashboard/dashboard.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import * as mapboxgl from 'mapbox-gl';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { CommonService } from '../../shared/common.service';
+import { ToastrService } from 'ngx-toastr';
+
 import { mapData } from '../../../assets/overlay_data';
 
 @Component({
@@ -25,8 +27,10 @@ export class DashboardComponent implements OnInit {
     public oauthService: OAuthService,
     public router: Router,
     public commonService: CommonService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService
   ) {
+    localStorage.removeItem('farmer-details');
     this.userInfo = this.oauthService.getIdentityClaims();
   }
 
@@ -66,7 +70,7 @@ export class DashboardComponent implements OnInit {
       mapViewType = 'existing_farmers_mapbox';
       //useData = mapData['features'];
       if (!this.allExistingFarmers) this.getExistingFarmers();
-      useData = this.allExistingFarmers;            
+      useData = this.allExistingFarmers;
     } else if (type == 'FARMS_PIPELINE_MAP_VIEW') {
       mapViewType = 'farms_pipeline_mapbox';
       //useData = mapData['features'];
@@ -83,35 +87,44 @@ export class DashboardComponent implements OnInit {
       zoom: 14, // starting zoom
       center: [77.73521840572359, 13.048329579932709],
     });
-    if(mapViewType === 'existing_farmers_mapbox'){
-    // geojson coordinates
-    map.on('load', () => {
-      useData.forEach((elem: any, index: number) => {        
-        // prepare popup
-        elem['fieldInfo'].forEach((f_elem: any, f_index: number) => {
-          let coordinates_arr = [] as any;
-          if(typeof(f_elem.field_boundary.geometry.coordinates[0][0]) === 'number'){
-            let coordinates_a = [] as any;
-            f_elem.field_boundary.geometry.coordinates.forEach(function (latlng: any, lli: number) {              
-              var ll_arr = [];
-              ll_arr.push(latlng[1]);
-              ll_arr.push(latlng[0]);
-              coordinates_a.push(ll_arr);
-            });
-            coordinates_arr.push(coordinates_a);
-          } else {          
-          let coordinates_a = [] as any;
-          f_elem.field_boundary.geometry.coordinates.forEach(function (latlng: any, lli: number) {
-              latlng.forEach(function (ll: any, li: number) {
+    if (mapViewType === 'existing_farmers_mapbox') {
+      // geojson coordinates
+      map.on('load', () => {
+        useData.forEach((elem: any, index: number) => {
+          // prepare popup
+          elem['fieldInfo'].forEach((f_elem: any, f_index: number) => {
+            let coordinates_arr = [] as any;
+            if (
+              typeof f_elem.field_boundary.geometry.coordinates[0][0] ===
+              'number'
+            ) {
+              let coordinates_a = [] as any;
+              f_elem.field_boundary.geometry.coordinates.forEach(function (
+                latlng: any,
+                lli: number
+              ) {
+                var ll_arr = [];
+                ll_arr.push(latlng[1]);
+                ll_arr.push(latlng[0]);
+                coordinates_a.push(ll_arr);
+              });
+              coordinates_arr.push(coordinates_a);
+            } else {
+              let coordinates_a = [] as any;
+              f_elem.field_boundary.geometry.coordinates.forEach(function (
+                latlng: any,
+                lli: number
+              ) {
+                latlng.forEach(function (ll: any, li: number) {
                   var ll_arr = [];
                   ll_arr.push(ll.lng);
                   ll_arr.push(ll.lat);
                   coordinates_a.push(ll_arr);
+                });
               });
-          });
-          coordinates_arr.push(coordinates_a);
-        }               
-          const popupDescription = `<div class="field_popup" style="width:260px;">
+              coordinates_arr.push(coordinates_a);
+            }
+            const popupDescription = `<div class="field_popup" style="width:260px;">
            <div class="row">
              <div class="col-md-6 text-left">
                <label class="fw-bold">Farmer Id</label>
@@ -136,75 +149,71 @@ export class DashboardComponent implements OnInit {
              <div class="col-md-6 text-left">
                <label class="fw-bold">Visit Land</label>
                <p class="text-capitalize">
-                 <a href="https://maps.google.com?q=${
-                   coordinates_arr[0][0][1]
-                 },${coordinates_arr[0][0][0]}
+                 <a href="https://maps.google.com?q=${coordinates_arr[0][0][1]},${coordinates_arr[0][0][0]}
                  " target="_blank">Take Me</a> </p> </div> </div>
-                   </div>`; 
+                   </div>`;
 
-        coordinates_arr.forEach((h: any, i: number) => {
-          // Add Source
-          map.addSource(`figure${i}_${index}_${f_index}`, {
-            type: 'geojson',
-            data: {
-              type: 'Feature',
-              geometry: {
-                type: 'Polygon',
-                coordinates: [h],
-              },
-              properties: {},
-            } as any,
+            coordinates_arr.forEach((h: any, i: number) => {
+              // Add Source
+              map.addSource(`figure${i}_${index}_${f_index}`, {
+                type: 'geojson',
+                data: {
+                  type: 'Feature',
+                  geometry: {
+                    type: 'Polygon',
+                    coordinates: [h],
+                  },
+                  properties: {},
+                } as any,
+              });
+
+              // Add a layer showing the fields.
+              map.addLayer({
+                id: `figure${i}_${index}_${f_index}`,
+                type: 'fill',
+                source: `figure${i}_${index}_${f_index}`,
+                layout: {},
+                paint: {
+                  'fill-outline-color': 'red',
+                  'fill-opacity': 1.0,
+                },
+              });
+              // const el = document.createElement('div');
+              // el.className = 'my-dash-marker';
+              // new mapboxgl.Marker(el).setLngLat(h[0]).addTo(map);
+              // When a click event occurs on a feature in the places layer, open a popup at the
+              // location of the feature, with description HTML from its properties.
+              map.on('click', `figure${i}_${index}_${f_index}`, (e) => {
+                new mapboxgl.Popup()
+                  .setLngLat(h[0])
+                  .setHTML(popupDescription)
+                  .setMaxWidth('400px')
+                  .addTo(map);
+              });
+
+              // Change the cursor to a pointer when the mouse is over the places layer.
+              map.on('mouseenter', `figure${i}_${index}_${f_index}`, () => {
+                map.getCanvas().style.cursor = 'pointer';
+              });
+
+              // Change it back to a pointer when it leaves.
+              map.on('mouseleave', `figure${i}_${index}_${f_index}`, () => {
+                map.getCanvas().style.cursor = '';
+              });
+            });
           });
 
-          // Add a layer showing the fields.
-          map.addLayer({
-            id: `figure${i}_${index}_${f_index}`,
-            type: 'fill',
-            source: `figure${i}_${index}_${f_index}`,
-            layout: {},
-            paint: {
-              'fill-outline-color': 'red',
-              'fill-opacity': 1.0,
-            },
-          });
-          // const el = document.createElement('div');
-          // el.className = 'my-dash-marker';
-          // new mapboxgl.Marker(el).setLngLat(h[0]).addTo(map);
-          // When a click event occurs on a feature in the places layer, open a popup at the
-          // location of the feature, with description HTML from its properties.
-          map.on('click', `figure${i}_${index}_${f_index}`, (e) => {
-            new mapboxgl.Popup()
-              .setLngLat(h[0])
-              .setHTML(popupDescription)
-              .setMaxWidth('400px')
-              .addTo(map);
-          });
-
-          // Change the cursor to a pointer when the mouse is over the places layer.
-          map.on('mouseenter', `figure${i}_${index}_${f_index}`, () => {
-            map.getCanvas().style.cursor = 'pointer';
-          });
-
-          // Change it back to a pointer when it leaves.
-          map.on('mouseleave', `figure${i}_${index}_${f_index}`, () => {
-            map.getCanvas().style.cursor = '';
-          });
-
+          // }
+          if (index == useData.length - 1) {
+            this.spinner.hide();
+          }
         });
-
-        }); 
-        
-        // }
-        if (index == useData.length - 1) {
-          this.spinner.hide();
-        }
       });
-    });
-  } else {
-    map.on('load', () => {
-      this.spinner.hide();
-    });
-  }
+    } else {
+      map.on('load', () => {
+        this.spinner.hide();
+      });
+    }
     map.addControl(new mapboxgl.NavigationControl()); // Add map controls
     map.addControl(new mapboxgl.FullscreenControl()); // Add map full screen
     setTimeout(function () {
@@ -286,6 +295,29 @@ export class DashboardComponent implements OnInit {
     );
   }
   /* END: API Function Calls */
+
+  getFarmerDetailsById(farmerId: any) {
+    localStorage.removeItem('farmer-details');
+    this.spinner.show();
+    this.commonService.getFarmerDetailsById(farmerId).subscribe(
+      (res: any) => {
+        this.spinner.hide();
+        if (res.message != 'Success' || !res.status) {
+          this.toastr.error(`${res.message}!`);
+        } else {
+          localStorage.setItem('farmer-details', JSON.stringify(res.data));
+          this.router.navigate([`/edit/demographic-info/${farmerId}`]);
+        }
+      },
+      (error: any) => {
+        this.spinner.hide();
+        this.toastr.error(
+          `Failed to fetch farmer details, please try again...`
+        );
+      }
+    );
+  }
+
   logOut() {
     this.oauthService.logOut();
     this.router.navigate(['/']);
--- src/app/edit-farmer/edit-header/edit-header.component.ts
+++ src/app/edit-farmer/edit-header/edit-header.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { OAuthService } from 'angular-oauth2-oidc';
 import { CommonService } from '../../shared/common.service';
 import { ActivatedRoute } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
@@ -14,76 +11,26 @@ import { Router, NavigationEnd } from '@angular/router';
 export class EditHeaderComponent implements OnInit {
   /* START: Variables */
   farmerId = '';
-  farmerData = {} as any;
   demographicDisp = {} as any;
-  currentRoute = '';
   /* END: Variables */
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private toastr: ToastrService,
-    public commonService: CommonService,
-    public oauthService: OAuthService,
-    private spinner: NgxSpinnerService,
-    private router: Router
+    private commonService: CommonService,
+    private activatedRoute: ActivatedRoute
   ) {
-    console.log(router.url);
     this.farmerId = this.activatedRoute.snapshot.params['farmerid'];
-    this.router.events.subscribe((event) => {
-      if (
-        event.constructor.name === 'NavigationEnd' ||
-        event instanceof NavigationEnd
-      ) {
-        const A = this.router.url.replace('/', '').split('/');
-        console.log(A);
 
-        if (A[0] === 'edit' && A[1] === 'demographic-info') {
-          localStorage.removeItem('farmer-details');
-          this.loadData();
-        } else {
-          const B = localStorage.getItem('farmer-details');
-          if (B) {
-            this.demographicDisp = JSON.parse(B).demographic_info;
-          }
-        }
-      }
-    });
+    const A: any = localStorage.getItem('farmer-details');
+    if (A) {
+      this.demographicDisp = JSON.parse(A).demographic_info;
+    }
   }
+
   ngOnInit(): void {}
   /* START: Non-API Function Calls */
-  loadData() {
-    this.getFarmerDetailsById(this.farmerId);
-  }
   // get Name from Master Json
   getDisplayName(dataProperty: string, id: any) {
     return this.commonService.getDisplayName('demoGraphic', dataProperty, id);
   }
   /* END: Non-API Function Calls */
-
-  /* START: API Function Calls */
-  getFarmerDetailsById(farmerId: any) {
-    this.spinner.show();
-    this.commonService.getFarmerDetailsById(farmerId).subscribe(
-      (res: any) => {
-        this.spinner.hide();
-        if (res.message != 'Success' || !res.status) {
-          this.toastr.error(`${res.message}!`);
-        } else {
-          this.farmerData = res.data;
-          localStorage.setItem(
-            'farmer-details',
-            JSON.stringify(this.farmerData)
-          );
-          this.demographicDisp = this.farmerData?.demographic_info;
-        }
-      },
-      (error: any) => {
-        this.spinner.hide();
-        this.toastr.error(
-          `Failed to fetch farmer details, please try again...`
-        );
-      }
-    );
-  }
-  /* END: API Function Calls */
 }
