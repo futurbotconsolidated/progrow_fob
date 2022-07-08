64b56e46cc4e1b1e8b9ed5ea90353a551185bce6
jitendrahostmudcom <96864144+jitendrahostmudcom@users.noreply.github.com>
2022-07-08 17:26:19 +0530

    Change redirect URI.

--- src/app/shared/authCodeFlowConfigpoc.ts
+++ src/app/shared/authCodeFlowConfigpoc.ts
@@ -3,10 +3,10 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 export const authCodeFlowConfig: AuthConfig = {
   issuer: 'https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_rr31W9OkL',
   loginUrl: `https://auth.poc.progrow.adaptiwise.com/oauth2/authorize`,
-  redirectUri: window.location.origin + '/home',
+  redirectUri: 'https://fob.poc.progrow.adaptiwise.com/home',
   clientId: '5bl9jg0r0dolhksblp49b8ifj2',
   logoutUrl:
-    'https://auth.poc.progrow.adaptiwise.com/logout?client_id=5bl9jg0r0dolhksblp49b8ifj2&response_type=code&scope=openid+profile&redirect_uri='+window.location.origin+'/login',
+    'https://auth.poc.progrow.adaptiwise.com/logout?client_id=5bl9jg0r0dolhksblp49b8ifj2&response_type=code&scope=openid+profile&redirect_uri=https://fob.poc.progrow.adaptiwise.com/login',
 
   scope: 'openid profile email',
   strictDiscoveryDocumentValidation: false,
--- src/app/shared/authCodeFlowConfigprod.ts
+++ src/app/shared/authCodeFlowConfigprod.ts
@@ -3,10 +3,10 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 export const authCodeFlowConfig: AuthConfig = {
   issuer: 'https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_IlarGnsQV',
   loginUrl: `https://auth1.agrisaathi.com/oauth2/authorize`,
-  redirectUri: window.location.origin + '/home',
+  redirectUri: 'https://fob.agrisaathi.com/home',
   clientId: '1806krf02bdr6a953e0mnk6l54',
   logoutUrl:
-    'https://auth1.agrisaathi.com/logout?client_id=1806krf02bdr6a953e0mnk6l54&response_type=code&scope=openid+profile&redirect_uri='+window.location.origin+'/login',
+    'https://auth1.agrisaathi.com/logout?client_id=1806krf02bdr6a953e0mnk6l54&response_type=code&scope=openid+profile&redirect_uri=https://fob.agrisaathi.com/login',
 
   scope: 'openid profile email',
   strictDiscoveryDocumentValidation: false,
